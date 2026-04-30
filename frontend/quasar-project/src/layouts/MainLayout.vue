77bd79e0e71cb400bbfa12beee3b0bf0329c12ee
Nac003 <bistricki.ignac1@gmail.com>
2026-04-30 02:01:37 +0200

    Dodan ADMIN gumb na pocetnu stranicu

--- frontend/quasar-project/src/layouts/MainLayout.vue
+++ frontend/quasar-project/src/layouts/MainLayout.vue
@@ -26,6 +26,14 @@
           icon="home"
         /> -->
         <q-btn
+          flat
+          color="white"
+          label="Admin"
+          icon="admin_panel_settings"
+          @click="$router.push('/PregledBiljaka')"
+        />
+
+        <q-btn
           href="https://www.agroklub.com/sortna-lista/"
           label="Agro klub"
           color="white"
